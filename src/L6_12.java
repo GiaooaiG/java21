9e62d5fb953d526ddd23a3ae103d2cdc49b2c4d6
Gyh <3499847578@qq.com>
2024-03-26 18:02:14 +0800

    20240326

---
+++ src/L6_12.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import test.Gyh;
+//显示日历
+public class L6_12 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        final int[] BASE = new int[]{1800,1,1};
+        final int BASE_WEEKDAY = 3;
+        //获取年份和月份
+        System.out.print("输入年份：");
+        int year = input.nextInt();
+        System.out.print("输入月份：");
+        int month = input.nextInt();
+        int[] date = new int[]{year,month,1};
+        int weekDay = Gyh.weekDay(BASE_WEEKDAY,Gyh.daysFrom(BASE,date));
+        for(int i = 0;i<=2;i++) {
+            if (BASE[i] > date[i]) {
+                weekDay = Gyh.weekDay(BASE_WEEKDAY, -1 * Gyh.daysFrom(date, BASE));
+                break;
+            }
+        }
+        printMonth(date,weekDay);
+    }
+    //打印日历
+    public static void printMonth(int[] date,int weekday){
+        System.out.println("          "+Gyh.nameOfMonth(date[1])+" "+date[0]);
+        System.out.println("---------------------------");
+        System.out.println("周日\t周一\t周二\t周三\t周四\t周五\t周六");
+        for(int i = 0;i < weekday;i++){
+            System.out.print("\t");
+        }
+        for(int i = 1;i<=Gyh.daysOfMonth(date[0],date[1]);i++){
+            System.out.print(i);
+            if((i+weekday)%7==0){
+                System.out.println();
+                continue;
+            }
+            System.out.print("\t");
+        }
+    }
+}
--- src/test/Gyh.java
+++ src/test/Gyh.java
@@ -12,14 +12,14 @@ public class Gyh {
     }
 
     //计算两个点之间的距离
-    public static double Distance(double[] arr1, double[] arr2) {
+    public static double distance(double[] arr1, double[] arr2) {
         double distance;
         distance = Math.sqrt(Math.pow(arr1[0] - arr2[0], 2) + Math.pow(arr1[1] - arr2[1], 2));
         return distance;
     }
 
     //计算最小公约数
-    public static int GreatestCommonDivisor(int n1, int n2) {
+    public static int greatestCommonDivisor(int n1, int n2) {
         int gcd=1;
         for (int k=2;k<=n1&&k<=n2;k++){
             if (n1%k==0&&n2%k==0){
@@ -27,5 +27,72 @@ public class Gyh {
             }
         }
         return gcd;
+    }
+
+    //输入星期和天数，计算星期几（周日为0）
+    public static int weekDay(int n1, int n2){
+        return (n1+n2)%7;
+    }
+
+    //计算两个时间相差的天数
+    public static int daysFrom(int[] date1,int[] date2){
+        int year1 = date1[0];
+        int year2 = date2[0];
+        int month1 = date1[1];
+        int month2 = date2[1];
+        int day1 = date1[2];
+        int day2 = date2[2];
+        int result = 0;
+
+        //计算开始年剩余天数
+        for(int i = month1;i <=12;i++){
+            result += daysOfMonth(year1,i);
+        }
+        result -= day1;
+        //计算中间的整年的天数
+        for(int i = year1 + 1;i < year2;i++){
+            result += isLeapYear(i) ? 366:365;
+        }
+        //计算结束年的天数
+        for(int i = 1;i < month2;i++) {
+            result += daysOfMonth(year2,i);
+        }
+        result += day2;
+
+        return result;
+    }
+
+    //判断是否为闰年
+    public static boolean isLeapYear(int n1){
+        return (n1 % 4 == 0 && n1 % 100 != 0) || (n1 % 400 == 0);
+    }
+
+    //返回每个月份的天数
+    public static int daysOfMonth(int year,int month){
+        return switch(month){
+            case 1,3,5,7,8,10,12 -> 31;
+            case 2 -> isLeapYear(year) ? 29:28;
+            case 4,6,9,11 -> 30;
+            default -> 0;
+        };
+    }
+
+    //返回每个月份的名字
+    public static String nameOfMonth(int month){
+        return switch(month){
+            case 1 -> "一月";
+            case 2 -> "二月";
+            case 3 -> "三月";
+            case 4 -> "四月";
+            case 5 -> "五月";
+            case 6 -> "六月";
+            case 7 -> "七月";
+            case 8 -> "八月";
+            case 9 -> "九月";
+            case 10 -> "十月";
+            case 11 -> "十一月";
+            case 12 -> "十二月";
+            default -> "";
+        };
     }
 }
